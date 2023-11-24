27e58bcb1f22cceb4b59cdabe335c934ce2469c0
Shuhei <sh7916@gmail.com>
2023-11-24 18:22:29 +0900

    fix github/openIssueLinkInNewTab

--- extension/scripts/github/openIssueLinkInNewTab.js
+++ extension/scripts/github/openIssueLinkInNewTab.js
@@ -1,6 +1,11 @@
 (async function () {
   retry(
     () => {
+      if (
+        !new URL(location.href).pathname.match("^/[^/]+/[^/]+/(issues|pulls)$")
+      )
+        return false;
+
       const links = Array.from(document.querySelectorAll(".Link--primary"));
       links
         .filter((link) => link.getAttribute("target") !== "_blank")
