7074251ee62cc106b01744b81660dff574b6958e
Shuhei <sh7916@gmail.com>
2026-05-17 12:04:20 +0900

    fix github/clickSsoButton

--- extension/scripts/github/clickSsoButton.js
+++ extension/scripts/github/clickSsoButton.js
@@ -3,10 +3,9 @@
     "Authenticate your account by logging into";
 
   retry(() => {
-    if (!new URL(location.href).pathname.match("^/orgs/[a-zA-Z0-9-]+/sso$")) return true;
-
     // 以下をチェックすることで、純粋なログイン時のみに自動クリックとし、SSHキーへの紐付け時等は自動クリックしないようにする
-    if (!document.body?.innerText.includes(authPrompt)) return false;
+    if (!document.body?.innerText.includes("Single sign-on to")) return false;
+    if (!document.body?.innerText.includes("Authenticate your account by logging into")) return false;
 
     const loginButton = document.querySelector("button[autofocus='autofocus']");
     if (!loginButton) return false;
